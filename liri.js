11ee093086e77f511b3c8a253a8dff7752c47911
Stephen McMillen <Shtevetm@gmail.com>
2019-10-02 21:48:14 -0500

    recreated liri.js with no inquirer prompt. Spotify default value was not returning Ace of Base. created function to return correct API response - hardcoded in 'index 5'. Default movie response is functional. installed chalk module to color responses.

--- liri.js
+++ liri.js
@@ -5,138 +5,108 @@ var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var inquirer = require("inquirer");
 var moment = require('moment');
+var chalk = require("chalk");
+var fs = require("fs");
 
 
+switch (process.argv[2]) {
 
-//=======USER PROMPTS=============//
-inquirer.prompt([
-    {
-        type: "list",
-        name: "type",
-        message: "Which would you like information about?",
-        choices: ["Spotify Music", "Concerts", "A Movie"],
-    }, {
-        type: "confirm",
-        name: "confirm",
-        message: "Are you sure?",
-        default: true,
-    },
-])
-    .then(function (response) {
-        if (response.confirm) {
-            switch (response.type) {
+    // =========================Spotify Music====================== //
+    case "spotify-this-song":
+        console.log(chalk.greenBright.bold(("SPOTIFY MUSIC SELECTED")));
+        var songTitle = process.argv[3];
+        songGet();
 
-                // =========================Spotify Music====================== //
-                case "Spotify Music":
-                    console.log("SPOTIFY MUSIC SELECTED");
-                    var songTitle = "";
-                    inquirer.prompt([
-                        {
-                            type: "input",
-                            name: "spotifyResp",
-                            message: "Please enter the name of a song",
-                        }
-                    ])
-                        .then(function (responseMusic) {
-                            songTitle = responseMusic.spotifyResp
-                            songGet();
-
-                            function songGet() {
-                                spotify
-                                    .search({ type: 'track', query: songTitle })
-                                    .then(function (respMus) {
-                                        console.log("Artist: " + respMus.tracks.items[0].artists[0].name);
-                                        console.log("Title: " + respMus.tracks.items[0].name);
-                                        console.log("Album: " + respMus.tracks.items[0].album.name);
-                                        console.log("Preview: " + respMus.tracks.items[0].preview_url);
-                                    })
-                                    .catch(function (err) {
-                                        console.log(err);
-                                    });
-                                // console.log(respMus.data.tracks);
-                                // console.log(respMus.data.tracks);
-                                // console.log(respMus.data.tracks);
-                                // console.log(respMus.data.tracks);
-
-                            }
-                        })
-                    break;
-                //==========================CONCERT==================//
-                case "Concerts":
-                    console.log("CONCERTS SELECTED");
-                    var bandName = "";
-                    inquirer.prompt([
-                        {
-                            type: "input",
-                            name: "bandResp",
-                            message: "Please enter the name of a artist or group",
-                        }
-                    ])
-                        .then(function (responseBand) {
-                            bandName = responseBand.bandResp
-                            concertGet();
-
-                            function concertGet() {
-                                var URL = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp"
-                                axios.get(URL)
-                                    .then(function (concertResult) {
-                                        for (var i = 0; i < 10; i++) {
-                                            // console.log("\n" + concertResult.data[i].datetime);
-                                            console.log(concertResult.data[i].venue.name);
-                                            console.log(concertResult.data[i].venue.city + ", " + concertResult.data[i].venue.region);
-                                            console.log("\n" + moment(concertResult.data[i].datetime).format('L'));
-                                        }
-                                    })
-                            }
-                        });
-                    break;
-
-
-
-                // =========================MOVIE====================== //
-                case "A Movie":
-                    console.log("MOVIE SELECTED");
-                    var movieTitle = "";
-                    inquirer.prompt([
-                        {
-                            type: "input",
-                            name: "movie",
-                            message: "Which movie would you like information about?",
-                        }
-                    ])
-                        .then(function (responseMovie) {
-                            movieTitle = responseMovie.movie
-                            movieGet();
-
-                            function movieGet() {
-                                axios.get("http://www.omdbapi.com/?t=" + movieTitle + "&apikey=trilogy")
-                                    .then(function (respMov) {
-                                        console.log("Title: " + respMov.data.Title);
-                                        console.log("Year: " + respMov.data.Year);
-                                        console.log("IMDB Rating: " + respMov.data.Ratings[0].Value);
-                                        console.log("Rotten Tomatoes Rating: " + respMov.data.Ratings[1].Value);
-                                        console.log("Country: " + respMov.data.Country);
-                                        console.log("Language: " + respMov.data.Language);
-                                        console.log("Plot: " + respMov.data.Plot);
-                                        console.log("Stars: " + respMov.data.Actors);
-                                    });
-                            }
-                        })
-                    break;
+        //============== BUILD FUNCTION =============//
+        function songGet() {
+            if (!songTitle) {
+                songTitle = "The Sign";
+                ace();
+            } else {
+                spotify
+                    .search({ type: 'track', query: songTitle })
+                    .then(function (respMus) {
+                        console.log((chalk.magentaBright.bold("Artist: ")) + respMus.tracks.items[0].artists[0].name);
+                        console.log((chalk.magentaBright.bold("Title: ")) + respMus.tracks.items[0].name);
+                        console.log((chalk.magentaBright.bold("Album: ")) + respMus.tracks.items[0].album.name);
+                        console.log((chalk.magentaBright.bold("Preview: ")) + respMus.tracks.items[0].preview_url);
+                    })
             }
-        } else {
-            console.log("Well try again when you're a little more decisive.")
+        };
+//=================
+        function ace() {
+            spotify
+                .search({ type: 'track', query: songTitle })
+                .then(function (respMus) {
+                    console.log((chalk.magentaBright.bold("Artist: ")) + respMus.tracks.items[5].artists[0].name);
+                    console.log((chalk.magentaBright.bold("Title: ")) + respMus.tracks.items[5].name);
+                    console.log((chalk.magentaBright.bold("Album: ")) + respMus.tracks.items[5].album.name);
+                    console.log((chalk.magentaBright.bold("Preview: ")) + respMus.tracks.items[5].preview_url);
+                })
+                }
+
+            
+
+break;
+    //==========================CONCERT==================//
+    case "concert-this":
+        console.log("CONCERT SELECTED");
+        var bandName = process.argv[3];
+        concertGet();
+
+        function concertGet() {
+            var URL = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp"
+            axios.get(URL)
+                .then(function (concertResult) {
+                    console.log((chalk.magentaBright.bold("UPCOMING SHOWS:")));
+                    for (var i = 0; i < 10; i++) {
+                        // console.log("\n" + concertResult.data[i].datetime);
+                        console.log((chalk.blue.bold("\n" + concertResult.data[i].venue.name)));
+                        console.log((chalk.green.bold(concertResult.data[i].venue.city + ", " + concertResult.data[i].venue.region)));
+                        console.log((chalk.yellow.bold(moment(concertResult.data[i].datetime).format('L'))));
+                    }
+                })
         }
-    });
+        break;
+    // =========================MOVIE====================== //
+    case "movie-this":
+        console.log((chalk.yellow.bold("MOVIE SELECTED")));
+        var movieTitle = process.argv[3];
+        movieGet();
 
-
-
-
-// ============IF STATEMENT(REPLACE WITH SWITCH STATEMENT)=========================//
-// if (response.type === "Music") {
-//     console.log("MUSIC SELECTED. RUN SPOTIFY API");
-// } else if (response.type === "Concerts") {
-//     console.log("CONCERTS SELECTED. RUN BANDS IN TOWN API");
-// } else {
-//     console.log("MOVIE SELECTED. RUN OMDB API")
-// }
+        function movieGet() {
+            if (!movieTitle) {
+                movieTitle = "Mr. Nobody";
+            }
+            axios.get("http://www.omdbapi.com/?t=" + movieTitle + "&apikey=trilogy")
+                .then(function (respMov) {
+                    console.log((chalk.blue.bold("Title: ") + respMov.data.Title));
+                    console.log((chalk.blue.bold("Year: ") + respMov.data.Year));
+                    console.log((chalk.blue.bold("IMDB Rating: ") + respMov.data.Ratings[0].Value));
+                    console.log((chalk.blue.bold("Rotten Tomatoes Rating: ") + respMov.data.Ratings[1].Value));
+                    console.log((chalk.blue.bold("Country: ") + respMov.data.Country));
+                    console.log((chalk.blue.bold("Language: ") + respMov.data.Language));
+                    console.log((chalk.blue.bold("Plot: ") + respMov.data.Plot));
+                    console.log((chalk.blue.bold("Stars: ") + respMov.data.Actors));
+                });
+        }
+        break;
+    //====================DO WHAT IT SAYS======================//
+    case "do-what-it-says":
+        console.log(chalk.greenBright.bold(("You're the boss...")));
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            if (error) {
+                console.log(error);
+            }
+            console.log(data);
+        })
+        break;
+    //==============================DEFAULT======================== //
+    default:
+        console.log(
+            "Please enter a command in the following formats" +
+            (chalk.magentaBright.bold("\n spotify-this-song '[SONG NAME]'" +
+                "\n concert-this '[BAND NAME]'" +
+                "\n movie-this '[MOVIE NAME]'" +
+                "\n do-what-it-says")));
+}
