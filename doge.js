88edd2ddfce2da22bed0ee362f027c28996c1d2e
Stephen McMillen <Shtevetm@gmail.com>
2019-10-02 23:04:49 -0500

    added doge.js - forked code from github of a ascii doge

---
+++ doge.js
@@ -0,0 +1,24 @@
+// I forked this from GitHub user hoserdude - https://gist.github.com/hoserdude //
+
+var chalk = require("chalk");
+
+
+console.log(chalk.gray.bgWhite("               ▄              ▄          "));
+console.log(chalk.gray.bgWhite("              ▌▒█           ▄▀▒▌         "));
+console.log(chalk.gray.bgWhite("    WOW       ▌▒▒█        ▄▀▒▒▒▐         "));
+console.log(chalk.gray.bgWhite("             ▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐         "));
+console.log(chalk.gray.bgWhite("           ▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐         "));
+console.log(chalk.gray.bgWhite("         ▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌         "));
+console.log(chalk.gray.bgWhite("        ▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒▌        "));
+console.log(chalk.gray.bgWhite("        ▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐        "));
+console.log(chalk.gray.bgWhite("       ▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌       "));
+console.log(chalk.gray.bgWhite("       ▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌       "));
+console.log(chalk.gray.bgWhite("      ▌▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒▐       "));
+console.log(chalk.gray.bgWhite("      ▐▒▒▐▀▐▀▒░▄▄▒▄▒▒▒▒▒▒░▒░▒░▒▒▒▒▌      "));
+console.log(chalk.gray.bgWhite("      ▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▐       "));
+console.log(chalk.gray.bgWhite("       ▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒░▒░▒░▒░▒▒▒▌       "));
+console.log(chalk.gray.bgWhite("       ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▄▒▒▐        "));
+console.log(chalk.gray.bgWhite("        ▀▄▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▄▒▒▒▒▌        "));
+console.log(chalk.gray.bgWhite("          ▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀         "));
+console.log(chalk.gray.bgWhite("            ▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀           "));
+console.log(chalk.gray.bgWhite("               ▒▒▒▒▒▒▒▒▒▒▀▀              "));
